ac83f84203d4a90b4ef487b6cd1c2ac340fe94f5
Ginura Premawardana <ginujj9@gmail.com>
2024-02-26 22:44:14 +0530

    Generate rondom image

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
--- app/src/main/java/com/example/flag_guessing_android_application/GuessCountry.kt
+++ app/src/main/java/com/example/flag_guessing_android_application/GuessCountry.kt
@@ -1,26 +1,43 @@
 package com.example.flag_guessing_android_application
 
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.example.flag_guessing_android_application.ui.theme.FlagguessingandroidapplicationTheme
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.util.Locale
+import kotlin.random.Random
 
 class GuessCountry : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -35,66 +52,243 @@ class GuessCountry : ComponentActivity() {
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun GuessCountryScreenContent() {
-    var text by remember { mutableStateOf("Hello Android!") }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.Center
-    ) {
-        // TopAppBar for navigation
-        TopAppBar(
-            title = {
-                Text(text = "Guess Country")
-            },
-            navigationIcon = {
-                IconButton(
-                    onClick = {
-                    }
-                ) {
-                    Icon(
-                        imageVector = Icons.Filled.ArrowBack,
-                        contentDescription = null
-                    )
-                }
-            },
-            actions = {
-                // Add additional actions here
-            }
-        )
-
-        // Your screen content
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun GuessCountryScreenContent() {
         Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.Blue)
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(
-                text = text,
-                modifier = Modifier.padding(16.dp)
+            // TopAppBar for navigation
+            TopAppBar(
+                title = {
+                    Text(text = "Guess Country")
+                },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            val NavigateGuessHint = Intent(this@GuessCountry,MainActivity::class.java)
+                            startActivity(NavigateGuessHint)
+                        },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = null
+                        )
+                    }
+                },
+                actions = {
+                    // Add additional actions here
+                },
             )
 
-            Button(
-                onClick = {
-                    // Example: Change the text on button click
-                    text = "Hello Compose!"
-                }
+            // Your screen content
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(204, 211, 202))
+                    .padding(vertical = 8.dp)
+
             ) {
-                Text(text = "Change Text")
+
+                Column (
+                    modifier = Modifier
+                        .weight(1f)
+                ){
+                    RandomImage()
+                }
+                Column (
+                    modifier = Modifier
+                        .weight(2f)
+                ){
+                    MyComposable()
+                }
+
             }
         }
     }
+
+
+    @Composable
+    fun MyComposable() {
+        val context = LocalContext.current
+        val countryMap = remember { mutableMapOf<String, String>() }
+        readJson(context, countryMap)
+
+        var isPressed by remember { mutableStateOf(false) }
+
+        // Now, you can use the countryMap as needed in your Compose code
+        // For example, you can print the content of the map
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 8.dp)
+
+//                .padding(vertical = 8.dp)
+
+
+//                .fillMaxHeight()
+        ) {
+            LazyColumn(
+                modifier = Modifier
+                    .weight(5f)
+                    .padding(vertical = 8.dp)
+//                    .height(300.dp)
+            ) {
+                items(countryMap.size) {
+                    countryMap.forEach { (key, value) ->
+
+                        Button(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(30),
+                            colors = ButtonDefaults.buttonColors(
+                                containerColor = Color(26, 93, 26)
+                            )
+                        )
+                        {
+                            Text(text = "$value")
+                        }
+                    }
+
+
+//
+//            val randomEntry = generateRandomEntry(countryMap)
+//
+//            Text(text = "${randomEntry.key}")
+//            Text(text = "${randomEntry.value}")
+//            Text(text = "${imageList.elementAt(1)}")
+                }
+            }
+            Button(
+                onClick = {isPressed = !isPressed },
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth(),
+                shape = RoundedCornerShape(20),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(17, 57, 70)
+                )
+
+
+            )
+            {
+                Text(text = if (isPressed) "Pressed" else "Submit")
+            }
+        }
+    }
+
+    @Composable
+    fun readJson(context: Context, countryMap: MutableMap<String, String>) {
+        // Read JSON from assets
+        try {
+            val json: String = context.assets.open("Countries.json").bufferedReader().use{it.readText() }
+
+            // Parse JSON using kotlinx.serialization
+            val countryDataList = Json.decodeFromString<Map<String, String>>(json)
+
+            // Update the countryMap
+            countryMap.putAll(countryDataList)
+        } catch (e: Exception) {
+            // Handle exceptions (e.g., JSON parsing errors)
+            // You may want to log the exception or show an error message
+            e.printStackTrace()
+        }
+    }
+
+
+
+    @Composable
+    fun RandomImage() {
+
+        val drawableList = listOf(
+
+            R.drawable.ad,
+            R.drawable.ae,
+            R.drawable.af,
+            R.drawable.ag,
+            R.drawable.ai,
+            R.drawable.al,
+            R.drawable.am,
+            R.drawable.ao,
+            R.drawable.aq,
+            R.drawable.ar,
+            R.drawable.at,
+            R.drawable.au,
+            R.drawable.aw,
+            R.drawable.ax,
+            R.drawable.az,
+            R.drawable.ba,
+            R.drawable.bb,
+            R.drawable.bd,
+            R.drawable.be,
+            R.drawable.bf,
+            R.drawable.bj,
+            R.drawable.bi,
+            R.drawable.bl,
+            R.drawable.bm,
+            R.drawable.bn,
+            R.drawable.bo,
+            R.drawable.bq,
+            R.drawable.br,
+            R.drawable.bs,
+            R.drawable.bt,
+            R.drawable.bv,
+            R.drawable.bw,
+            R.drawable.by,
+            R.drawable.bz,
+            R.drawable.ca,
+            R.drawable.cc,
+            R.drawable.cd,
+            R.drawable.cf,
+            R.drawable.cg,
+            R.drawable.ch,
+            R.drawable.ci,
+            R.drawable.ck,
+            R.drawable.cl,
+            R.drawable.cm,
+            R.drawable.cn,
+            R.drawable.co,
+            R.drawable.cr,
+            R.drawable.cu,
+            R.drawable.cv,
+            R.drawable.cw,
+            R.drawable.cx,
+            R.drawable.cy,
+            R.drawable.cz,
+        )
+        val randomDrawableId = drawableList.random()
+
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .clip(MaterialTheme.shapes.medium),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                painter = painterResource(id = randomDrawableId),
+                contentDescription = "Country Flag",
+
+
+                contentScale = ContentScale.Crop
+            )
+        }
+
+    }
+
+
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GuessCountryScreenPreview() {
-    FlagguessingandroidapplicationTheme {
-        GuessCountryScreenContent()
-    }
-}
+
+
+
+//fun readJson(){
+//    var json : String? = null
+//    val inputStream: InputStream = assets.open("Countries.json")
+//    json = inputStream.bufferedReader ().use(it.readText())
+//}
+
+
